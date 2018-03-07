24aeddb7cb010378879cd19bcfad2db604a78533
Shameera Rakiep <shameerarakiep@gmail.com>
2018-03-07 16:04:47 +0200

    using geolocation to find users current location, this will then pil all places closest to where the user is at that point, made layout changes with a loader for waiting until position is aquired, side bar with place details still to come

--- frontend/places.js
+++ frontend/places.js
@@ -1,41 +1,54 @@
 var map;
 var infoWindow;
 var allPlaces = [];
-var request;
-var service;
-var markers = [];
+var currentlocation = [];
+
+var center = new google.maps.LatLng(0, 0);
+
+
+
+
+
 function initialize() {
-    var center = new google.maps.LatLng(-33.9152208, 18.3758741);
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: center,
-        zoom: 13
-    });
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            console.log(position.coords.toString())
+            if (position) {
+                document.getElementById('loadingScreen').style.display = 'none';
+            }
+            center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map = new google.maps.Map(document.getElementById('map'), {
+                center: center,
+                zoom: 13
+            });    
+        
+        
+            var request = {
+                location: center,
+                radius: 8047,
+                types: ['cafe']
+            };    
+            infoWindow = new google.maps.InfoWindow();
+        
+            var service = new google.maps.places.PlacesService(map);
+        
+            service.nearbySearch(request, callback);
+            // currentlocation.push({
+            //     lat: position.coords.latitude, 
+            //     lng: position.coords.longitude
+            // });
+        }, function(failure) {
+            if(failure.message.indexOf("Only secure origins are allowed") == 0) {
+              // Secure Origin issue.
+              console.log(failure);
+              
+            }
+          })
+    } else {
+        alert("No Geolocation")
+    }
 
-
-    request = {
-        location: center,
-        radius: 8047,
-        types: ['cafe']
-    };
-    infoWindow = new google.maps.InfoWindow();
-
-    service = new google.maps.places.PlacesService(map);
-
-    service.nearbySearch(request, callback);
-
-    google.maps.event.addListener(map, 'rightclick', function(event){
-        map.setCenter(event.latLng)
-        clearResults(markers)
-
-        var request = {
-            location: event.latLng,
-            radius: 8047,
-            types: ['cafe']
-        };
-        service.nearbySearch(request, callback);
-    })
-
-}
+}    
 
 function callback(results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
@@ -43,56 +56,44 @@ function callback(results, status) {
 
         for (var i = 0; i < results.length; i++) {
 
-            markers.push(createMarker(results[i]));
-        }
-    }
-}
+            createMarker(results[i]);
+        }    
+        console.log("all places", allPlaces)
+    }    
+}    
 
 
 function createMarker(place) {
     var placeLoc = place.geometry.location;
-    marker = new google.maps.Marker({
+    var marker = new google.maps.Marker({
         map: map,
         position: place.geometry.location
-    });
+    });    
     google.maps.event.addListener(marker, 'click', function () {
-        infoWindow.setContent('<a data-target="modal1" class="btn modal-trigger">' + place.name + '</a>');
+        infoWindow.setContent('<a href="///Users/nelsonmgengwana/projects/placeFrontend/place.html">' + place.name + '</a>');
         infoWindow.open(map, this);
-    });
-    return marker;
+    });    
     allPlaces.push(place);
+}    
+
+function placeMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 13
+    });
+
+
+    var request = {
+        location: center,
+        radius: 8047,
+        types: ['cafe']
+    };
+    
+    var service = new google.maps.places.PlacesService(map);
+
+    service.nearbySearch(request, callback);
+
 }
-function clearResults(markers){
-    for (var m in markers) {
-        markers[m].setMap(null)
-    }
-    markers = [];
-}
-
-
-
-
-
-
-// var getOnePLace = function () {
-//     var searchPlace = document.getElementById('search').value;
-//     console.log("THIS IS THE INPUT VALUE", searchPlace)
-//     var onePlace = [];
-//     if (searchPlace.trim().length > 0) {
-//         allPlaces.forEach(
-//             function (item) {
-//                 if (item.name.toLowerCase() === searchPlace.toLowerCase().trim()) {
-//                     onePlace.push(item);
-//                     console.log("this is the item to be pushed to array", item)
-
-//                 }
-//             });
-//     }
-//     return onePlace;
-//     console.log("THIS IS THE ONE PLACE TO BE RETURNED WHE SEARCHED", onePLace)
-
-
-// };
 
 $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
@@ -100,3 +101,4 @@ $(document).ready(function () {
 });
 
 google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', placeMap);
