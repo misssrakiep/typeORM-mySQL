df277b6a9952be06aef1931cedbb26263ab47a56
Vaztonyo <tony.dewet7@gmail.com>
2018-03-14 12:02:38 +0200

    got the card and map displaying in the same row but still working on the layout

--- place-front/main.js
+++ place-front/main.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
-function createMap() {
-    var mapProp= {
-        center:new google.maps.LatLng(-34.000836,18.459778),
-        zoom:11,
-    };
-    var map=new google.maps.Map(document.getElementById("map"),mapProp);
+    var map;
+    function createMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -33.918861, lng: 18.423300},
+        zoom: 11.5
+      });
     }
     createMap();
 });
