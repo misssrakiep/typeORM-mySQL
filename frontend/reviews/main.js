12a01683170046ddfb7e790f24558b846984bab5
Shameera Rakiep <shameerarakiep@gmail.com>
2018-03-25 13:45:24 +0200

    changed file structure and added a landing page

--- frontend/reviews/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $.ajax({
-        headers: { "Accept": "application/json"},
-        type: "GET",
-        url: "",
-        success: function() { alert("Success"); },
-        error: function() { alert('Failed!'); }
-    })
-});
